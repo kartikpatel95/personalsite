6c81fc3ef892a62c716c1e0bae1ccebfd2560173
Kartik Patel <kartikpatel@outlook.co.nz>
2019-09-01 18:38:03 +1200

    profile data

--- app/src/Personal/Extensions/CustomSiteConfig_Extensions.php
+++ app/src/Personal/Extensions/CustomSiteConfig_Extensions.php
@@ -8,9 +8,40 @@
 
 namespace Personal {
 
+    use SilverStripe\Forms\DateField;
+    use SilverStripe\Forms\EmailField;
+    use SilverStripe\Forms\FieldList;
+    use SilverStripe\Forms\TextareaField;
+    use SilverStripe\Forms\TextField;
     use SilverStripe\ORM\DataExtension;
 
+    /**
+     * Class CustomSiteConfig_Extensions
+     * @package Personal
+     * @property string $Position
+     * @property string $PhoneNumber
+     * @property string $Email
+     * @property string $Address
+     * @property Date $DateOfBirth
+     */
     class CustomSiteConfig_Extensions extends DataExtension {
+        private static $db = [
+            "Position" => 'Varchar(128)',
+            'PhoneNumber' => 'Varchar(64)',
+            'Email' => 'Varchar(256)',
+            'Address' => 'Text',
+            'DateOfBirth' => 'Date'
+        ];
 
+        public function updateCMSFields(FieldList $fields)
+        {
+            $fields->addFieldsToTab('Root.Main', [
+                TextField::create('Position', 'Work Position'),
+                TextField::create('PhoneNumber', 'Phone Number'),
+                EmailField::create('Email', 'Email Address'),
+                TextareaField::create('Address', 'Physical Address'),
+                DateField::create('DateOfBirth', 'Date of Birth')
+            ]);
+        }
     }
 }
