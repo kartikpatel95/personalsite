c2982b76cc11deb068c3f25c024da6d5be5ed233
Kartik Patel <kartikpatel@outlook.co.nz>
2020-01-08 22:59:34 +1300

    extra font sizes added to RTE

--- app/_config.php
+++ app/_config.php
@@ -8,5 +8,5 @@ HtmlEditorConfig::get('cms')->insertButtonsBefore('formatselect', 'backcolor');
 HtmlEditorConfig::get('cms')->insertButtonsBefore('formatselect', 'fontsizeselect');
 
 HtmlEditorConfig::get('cms')->setOptions([
-    'fontsize_formats' => '8pt 10pt 11pt 12pt 14pt 18pt 24pt 36pt'
+    'fontsize_formats' => '8pt 10pt 11pt 12pt 14pt 18pt 24pt 36pt 54pt 72pt'
 ]);
